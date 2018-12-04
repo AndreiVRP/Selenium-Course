e2ef65dab4cd4a060ebc5cea9f94f64dc3f99f0f
AndreiVRP <andrei.hrabovich.vrp@gmail.com>
2018-12-04 21:45:32 +0300

    Lesson 42. Find Element (basics).

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -15,12 +17,23 @@ public class Main {
         chromeDriver.manage().window().setSize(new Dimension(1280,720));
 
         chromeDriver.get("https://www.google.com");
-        chromeDriver.navigate().to("https://www.youtube.com");
-        chromeDriver.navigate().back();
-        chromeDriver.navigate().forward();
-        chromeDriver.navigate().refresh();
+//        chromeDriver.navigate().to("https://www.youtube.com");
+//        chromeDriver.navigate().back();
+//        chromeDriver.navigate().forward();
+//        chromeDriver.navigate().refresh();
+//        chromeDriver.navigate().back();
+        WebElement gmailLink = chromeDriver.findElement(By.linkText("Gmail"));
+        gmailLink.click();
+        WebElement more = chromeDriver.findElement(By.partialLinkText("ПОДРОБНЕЕ"));
+        more.click();
         System.out.println(chromeDriver.getTitle());
         System.out.println(chromeDriver.getCurrentUrl());
+        chromeDriver.navigate().back();
+//        WebElement appstore = chromeDriver.findElement(By.id("gmail-devices-appstore-button"));
+//        WebElement appstore = chromeDriver.findElement(By.xpath("//a[@id=\"gmail-devices-appstore-button\"]"));
+        WebElement appstore = chromeDriver.findElement(By.cssSelector("#gmail-devices-appstore-button"));
+//        appstore.click(); // this is not clickable - need to find the real button link behind it
+
 //        chromeDriver.close(); // difference between close() and quit()
         chromeDriver.quit();
     }
