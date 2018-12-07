973b07659009eda3ea928d896ea1e88057711abd
AndreiVRP <andrei.hrabovich.vrp@gmail.com>
2018-12-07 15:55:16 +0300

    Lessons 54-55. Working with multiple windows and tabs.

--- src/main/java/Main5.java
+++ src/main/java/Main5.java
@@ -27,13 +27,34 @@ public class Main5 {
 
         chromeDriver.get("https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html");
         chromeDriver.findElement(By.xpath("//a[text()=\"jdk-8u192-macosx-x64.dmg\"]")).click();
+        pauseFor(1);
+        chromeDriver.switchTo().alert().dismiss();
+
+        //working with different windows
+        String mainWindow = chromeDriver.getWindowHandle(); //this is the initial browser window
+        chromeDriver.findElement(By.xpath("//a[contains(text(),\"License Agreement\") and contains(@href,\"192\")]")).click();
+        switchToNewWindow(chromeDriver);
+
+        //now you can interact with the web page open in this window
+        WebElement contactMenu = chromeDriver.findElement(By.xpath("//a[contains(@href,\"contactmenu\")]"));
+        actions.moveToElement(contactMenu).click().build().perform();
+        pauseFor(1);
+        chromeDriver.switchTo().window(mainWindow); // navigating back to the initial window
+        pauseFor(1);
+        chromeDriver.quit();
+    }
+
+    public static void pauseFor(int seconds) {
         try {
-            Thread.sleep(3000);
+            Thread.sleep(seconds * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        chromeDriver.switchTo().alert().dismiss();
+    }
 
-        chromeDriver.quit();
+    public static void switchToNewWindow(WebDriver driver) {
+        for (String windowHandle : driver.getWindowHandles()) {
+            driver.switchTo().window(windowHandle); // this is to finally get to the most recent (new) window (or tab)
+        }
     }
 }
