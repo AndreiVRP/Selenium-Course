c3c51fac9c282ddf7058fb27a627a568830db9b8
AndreiVRP <andrei.hrabovich.vrp@gmail.com>
2018-12-07 13:11:29 +0300

    Lesson 50. Explicit Wait.

--- src/main/java/Main3.java
+++ src/main/java/Main3.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -9,11 +11,13 @@ public class Main3 {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "webDrivers/chromedriver");
         WebDriver chromeDriver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(chromeDriver, 5);
+
         chromeDriver.manage().window().fullscreen();
         chromeDriver.get("https://www.facebook.com/");
         chromeDriver.findElement(By.xpath("//select[@title=\"Year\"]")).click();
-//        chromeDriver.findElement(By.xpath("//select[@title=\"Year\"]//option[@value=\"2018\"]")).click();
-        chromeDriver.findElement(By.xpath("//option[@value=\"1985\"]")).click();
+        WebElement birthYear = chromeDriver.findElement(By.xpath("//option[@value=\"1985\"]"));
+        wait.until(ExpectedConditions.visibilityOf(birthYear)).click();
         chromeDriver.findElement(By.xpath("//input[@name=\"firstname\"]")).click();
         List<WebElement> footerLinks = chromeDriver.findElements(By.xpath("//ul[contains(@class, \"Footer\")]/li"));
         for (WebElement link : footerLinks) {
