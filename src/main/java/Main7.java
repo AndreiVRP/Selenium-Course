e0053c8196982572318b416c93117795c9da24e8
AndreiVRP <andrei.hrabovich.vrp@gmail.com>
2018-12-10 15:54:07 +0300

    Lesson 59. Taking screenshots.

--- src/main/java/Main7.java
+++ src/main/java/Main7.java
@@ -1,9 +1,12 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main7 {
     public static void main(String[] args) {
@@ -11,16 +14,29 @@ public class Main7 {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().fullscreen();
         driver.get("https://www.google.com/");
+        takeScreenshot(driver);
         WebElement searchInput = driver.findElement(By.xpath("//input[@aria-label=\"Search\"]"));
         Actions actions = new Actions(driver);
-
         actions.moveToElement(searchInput)
                 .sendKeys("12345qwerty12345")
                 .doubleClick()
-                .sendKeys("56789")
+                .sendKeys("56789!")
                 .sendKeys(Keys.chord(Keys.COMMAND, "a"))
                 .build().perform();
-
+        takeScreenshot(driver);
         driver.quit();
+    }
+
+    public static void takeScreenshot(WebDriver driver) {
+        // taking screenshots
+        Date dateNow = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("hh_mm_ss");
+        String fileName = format.format(dateNow) + ".png";
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenshot, new File("src/screenshots/" + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
